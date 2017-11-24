48dc9031179189f1937d0f0b0f18a152cd5dec25
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-11-24 05:40:11 +0200

    Get rid of unittest, use pytest

--- boardealis/tests.py
+++ boardealis/tests.py
@@ -1,15 +1,15 @@
-import unittest
-
-from pyramid import testing
+import pytest
 
 
-class FunctionalTests(unittest.TestCase):
-    def setUp(self):
+class TestsFunctional(object):
+    # pylint:disable=no-self-use
+
+    @pytest.fixture
+    def app(self):
         from boardealis import main
-        app = main({})
         from webtest import TestApp
-        self.testapp = TestApp(app)
+        return TestApp(main({}))
 
-    def test_home(self):
-        res = self.testapp.get('/', status=200)
+    def test_home(self, app):
+        res = app.get('/', status=200)
         assert b'Boardealis' in res.body
