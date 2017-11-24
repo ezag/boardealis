cd15f6369d385f4090883c9fed05bc1e23edb9e3
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-11-24 21:57:37 +0200

    Redirect to slash-appended routes

--- boardealis/__init__.py
+++ boardealis/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from pyramid.httpexceptions import HTTPNotFound
 
 
 def main(global_config, **settings):  # pylint:disable=unused-argument
@@ -6,6 +7,7 @@ def main(global_config, **settings):  # pylint:disable=unused-argument
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('login', '/-/login')
+    config.add_route('login', '/-/login/')
+    config.add_notfound_view(lambda request: HTTPNotFound(), append_slash=True)
     config.scan()
     return config.make_wsgi_app()
--- boardealis/tests.py
+++ boardealis/tests.py
@@ -13,4 +13,10 @@ class TestsFunctional(object):
     def test_home(self, app):
         res = app.get('/', status=200)
         assert 'Добро пожаловать' in res.lxml.xpath('//title/text()')[0]
-        assert '/-/login' in res.lxml.xpath('//a/@href')
+        assert '/-/login/' in res.lxml.xpath('//a/@href')
+
+    @pytest.mark.xfail
+    def test_login(self, app):
+        res = app.get('/-/login', status=302)
+        res = app.get('/-/login/', status=200)
+        assert 'Вход' in res.lxml.xpath('//title/text()')[0]
