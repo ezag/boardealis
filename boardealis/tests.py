229e0918efb8c324166313e4b22ca6d8332e550f
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-11-24 05:29:34 +0200

    Make trivial homepage test pass

--- boardealis/tests.py
+++ boardealis/tests.py
@@ -3,20 +3,6 @@ import unittest
 from pyramid import testing
 
 
-class ViewTests(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'boardealis')
-
-
 class FunctionalTests(unittest.TestCase):
     def setUp(self):
         from boardealis import main
@@ -24,6 +10,6 @@ class FunctionalTests(unittest.TestCase):
         from webtest import TestApp
         self.testapp = TestApp(app)
 
-    def test_root(self):
+    def test_home(self):
         res = self.testapp.get('/', status=200)
-        self.assertTrue(b'Pyramid' in res.body)
+        assert b'Boardealis' in res.body
