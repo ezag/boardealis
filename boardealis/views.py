50b64b0a63516161460f751e569673eb42a9d03f
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-11-24 05:41:09 +0200

    PEP8 blank line fix

--- boardealis/views.py
+++ boardealis/views.py
@@ -5,6 +5,7 @@ from pyramid.view import view_config
 def home(request):  # pylint:disable=unused-argument
     return {}
 
+
 @view_config(route_name='login', renderer='templates/login.mako')
 def login(request):  # pylint:disable=unused-argument
     return {}
