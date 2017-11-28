a80f720af555a3b130c07d3b8808b01f495be812
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-11-29 00:21:28 +0200

    Use velruse for login via GitHub

--- boardealis/views.py
+++ boardealis/views.py
@@ -1,4 +1,5 @@
 from pyramid.view import view_config
+from velruse import login_url
 
 
 @view_config(route_name='home', renderer='templates/home.mako')
@@ -8,4 +9,4 @@ def home(request):  # pylint:disable=unused-argument
 
 @view_config(route_name='login', renderer='templates/login.mako')
 def login(request):  # pylint:disable=unused-argument
-    return {}
+    return dict(login_url=login_url)
