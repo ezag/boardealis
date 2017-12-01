06c85f7ab7c75502d7ffe7a7cdbec8fb41d9ec7d
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-12-01 10:38:47 +0200

    Generalize config.add_*_login_from_settings calls

--- boardealis/__init__.py
+++ boardealis/__init__.py
@@ -6,8 +6,12 @@ from pyramid.session import SignedCookieSessionFactory
 def main(global_config, **settings):  # pylint:disable=unused-argument
     config = Configurator(settings=settings)
     config.set_session_factory(SignedCookieSessionFactory(settings['session.secret']))
-    config.add_facebook_login_from_settings()
-    config.add_github_login_from_settings()
+    for provider in [
+        provider.strip().replace('velruse.providers.', '')
+        for provider in settings['pyramid.includes'].split('\n')
+        if provider.strip().startswith('velruse.providers.')
+    ]:
+        getattr(config, 'add_{}_login_from_settings'.format(provider))()
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('login', '/-/login/')
