b5df6a359a8ebb91136e25ea9b8213e19d7344a9
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-12-01 06:32:53 +0200

    Refine config

--- boardealis/__init__.py
+++ boardealis/__init__.py
@@ -5,11 +5,8 @@ from pyramid.session import SignedCookieSessionFactory
 
 def main(global_config, **settings):  # pylint:disable=unused-argument
     config = Configurator(settings=settings)
-    config.include('pyramid_mako')
-    config.include('velruse.providers.github')
-    # TODO: take session secret from config
-    config.set_session_factory(SignedCookieSessionFactory('itsaseekreet'))
-    config.add_github_login_from_settings(prefix='velruse.github.')
+    config.set_session_factory(SignedCookieSessionFactory(settings['session.secret']))
+    config.add_github_login_from_settings()
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('login', '/-/login/')
