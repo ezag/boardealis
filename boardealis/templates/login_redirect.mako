9e7f9b869acf1fcecb7c02e0b3d6111f5b5cf4df
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-12-05 15:27:19 +0200

    Show token on auth success

--- boardealis/templates/login_redirect.mako
+++ boardealis/templates/login_redirect.mako
@@ -1,7 +1,16 @@
 <%inherit file="layout.mako"/>
 <%block name="title">Вход &middot; ${parent.title()}</%block>
 <div class="text-center">
+% if token is not None:
 <p class="lead">
-Входим...
+Входим... <code>${token}</code>
 </p>
+% else:
+<p class="lead">
+Ой, что-то пошло не так!
+</p>
+<p>
+<a role="button" class="btn btn-lg btn-warning" href="${request.route_url('login')}">Попробовать снова</a>
+</p>
+% endif
 </div>
--- boardealis/views.py
+++ boardealis/views.py
@@ -1,6 +1,10 @@
+import logging
+
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid.view import view_config
 from requests_oauthlib import OAuth2Session
 
+log = logging.getLogger(__name__)
 
 @view_config(route_name='home', renderer='templates/home.mako')
 def home(request):  # pylint:disable=unused-argument
@@ -24,5 +28,20 @@ def login(request):
 
 
 @view_config(route_name='login_redirect', renderer='templates/login_redirect.mako')
-def login_redirect(request, provider):  # pylint:disable=unused-argument
-    return {}
+def login_redirect(request):
+    settings = request.registry.settings
+    provider = request.matchdict['provider']
+    if provider not in settings['oauth.providers'].split():
+        raise HTTPNotFound()
+    try:
+        oauth = OAuth2Session(
+            settings['oauth.{}.client_id'.format(provider)],
+            state=request.session['oauth_state'])
+        token = oauth.fetch_token(
+            settings['oauth.{}.token_url'.format(provider)],
+            code=request.GET['code'],
+            client_secret=settings['oauth.{}.client_secret'.format(provider)])['access_token']
+    except Exception:
+        log.exception('Failed to get OAuth token from %s', provider)
+        token = None
+    return dict(token=token)
