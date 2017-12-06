59daf1839d2aea003248e6edd4f04c7ac3ed0f59
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2017-12-06 05:21:47 +0200

    Add login via Facebook

--- boardealis/views.py
+++ boardealis/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from oauthlib.common import generate_token
 from oauthlib.oauth2 import MismatchingStateError
 from pyramid.httpexceptions import HTTPBadRequest, HTTPNotFound
 from pyramid.view import view_config
@@ -16,16 +17,17 @@ def home(request):  # pylint:disable=unused-argument
 @view_config(route_name='login', renderer='templates/login.mako')
 def login(request):
     settings = request.registry.settings
+    request.session['oauth_state'] = generate_token()
     auth_links = []
     for provider in settings['oauth.providers'].split():
         oauth = OAuth2Session(
             settings['oauth.{}.client_id'.format(provider)],
+            state=request.session['oauth_state'],
             redirect_uri=request.route_url('login_redirect', provider=provider),
-            scope=settings['oauth.{}.scope'.format(provider)])
-        url, state = oauth.authorization_url(
+            scope=settings.get('oauth.{}.scope'.format(provider)))
+        url, _state = oauth.authorization_url(
             settings['oauth.{}.authorization_url'.format(provider)])
         auth_links.append((settings['oauth.{}.title'.format(provider)], url))
-        request.session['oauth_state'] = state
     return dict(auth_links=auth_links)
 
 
@@ -39,22 +41,33 @@ def login_redirect(request):
         raise HTTPBadRequest(MismatchingStateError().description)
     oauth = OAuth2Session(
         settings['oauth.{}.client_id'.format(provider)],
-        state=request.session['oauth_state'])
+        state=request.session['oauth_state'],
+        redirect_uri=request.route_url('login_redirect', provider=provider))
     try:
-        token = oauth.fetch_token(
+        oauth.fetch_token(
             settings['oauth.{}.token_url'.format(provider)],
             authorization_response=request.url,
             client_secret=settings['oauth.{}.client_secret'.format(provider)])
     except MismatchingStateError as exc:
         raise HTTPBadRequest(exc.description)
-    profile = oauth.get('https://api.github.com/user').json()
-    emails = oauth.get('https://api.github.com/user/emails').json()
-    result = dict(
-        success=True,
-        token=token['access_token'],
-        name=profile['name'],
-        avatar_url=profile['avatar_url'] + '&s=256',
-        email=[entry['email'] for entry in emails if entry['primary']][0],
-        provider=settings['oauth.{}.title'.format(provider)],
-    )
+    if provider == 'github':
+        profile = oauth.get('https://api.github.com/user').json()
+        emails = oauth.get('https://api.github.com/user/emails').json()
+        result = dict(
+            success=True,
+            name=profile['name'],
+            avatar_url=profile['avatar_url'] + '&s=256',
+            email=[entry['email'] for entry in emails if entry['primary']][0],
+            provider=settings['oauth.{}.title'.format(provider)],
+        )
+    elif provider == 'facebook':
+        profile = oauth.get(
+            'https://graph.facebook.com/v2.11/me?fields=id,name,email,picture').json()
+        result = dict(
+            success=True,
+            name=profile['name'],
+            avatar_url=profile['picture']['data']['url'],
+            email=profile.get('email', 'NO EMAIL'),
+            provider=settings['oauth.{}.title'.format(provider)],
+        )
     return result
